7ff090b1638cb6d882dcbc5b20ec887664198a8e
Deekshith Shetty <deekshithshetty698@gmail.com>
2024-07-02 21:10:11 +0530

    Finish Hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Nav from "./components/Nav";
+import Hero from "./sections/Hero";
 
 function App() {
   return (
     <main className="relative">
       <Nav />
-      <section className="xl:padding-l wide:padding-r padding-b">Hero</section>
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
       <section className="padding">PopularProducts</section>
       <section className="padding">SuperQuality</section>
       <section className="padding-x py-10">Services</section>
