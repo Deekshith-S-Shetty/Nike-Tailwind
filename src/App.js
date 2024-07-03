06d82472621cef4417bcfe2a18175682de6f4d63
Deekshith Shetty <deekshithshetty698@gmail.com>
2024-07-03 21:10:39 +0530

    Add and design popular products section for the application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Nav from "./components/Nav";
 import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
 
 function App() {
   return (
     <main className="relative">
       <Nav />
-      <section className="xl:padding-l wide:padding-r padding-b">
+      <section className="xl:padding-l wide:padding-r padding-b padding-t">
         <Hero />
       </section>
-      <section className="padding">PopularProducts</section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
       <section className="padding">SuperQuality</section>
       <section className="padding-x py-10">Services</section>
       <section className="padding">SpecialOffer</section>
