43c0c3bef059a580079d7f0f8d0818fcb871e139
Deekshith Shetty <deekshithshetty698@gmail.com>
2024-07-03 21:28:54 +0530

    Add SuperQuality section with design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import Hero from "./sections/Hero";
 import PopularProducts from "./sections/PopularProducts";
+import SuperQuality from "./sections/SuperQuality";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <section className="padding">
         <PopularProducts />
       </section>
-      <section className="padding">SuperQuality</section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
       <section className="padding-x py-10">Services</section>
       <section className="padding">SpecialOffer</section>
       <section className="bg-pale-blue padding">CustomerReviews</section>
