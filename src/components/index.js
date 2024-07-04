847098d72286c84b2ef94abf984182b8819bc7f3
Deekshith Shetty <deekshithshetty698@gmail.com>
2024-07-04 14:36:53 +0530

    Add Customer Reviews for the application

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,14 @@ import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
 import Services from "../sections/Services";
 import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 
-export { Button, Nav, ShoeCard, PopularProductCard, Services, ServiceCard };
+export {
+  Button,
+  Nav,
+  ShoeCard,
+  PopularProductCard,
+  Services,
+  ServiceCard,
+  ReviewCard,
+};
